c6fc5447c12f50fd25cbd9780768c7ab60cf19d2
Klimov Paul <klimov.paul@gmail.com>
2015-07-06 11:55:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+StaticDB Extension for Yii 2
+============================
+
+This extension provides ActiveRecord interface for the data declared in static PHP files.
+
+For license information check the [LICENSE](LICENSE.md)-file.
+
+[![Latest Stable Version](https://poser.pugx.org/yii2tech/staticdb/v/stable.png)](https://packagist.org/packages/yii2tech/staticdb)
+[![Total Downloads](https://poser.pugx.org/yii2tech/staticdb/downloads.png)](https://packagist.org/packages/yii2tech/staticdb)
+[![Build Status](https://travis-ci.org/yii2tech/staticdb.svg?branch=master)](https://travis-ci.org/yii2tech/staticdb)
+
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist yii2tech/staticdb
+```
+
+or add
+
+```json
+"yii2tech/staticdb": "*"
+```
+
+to the require section of your composer.json.
+
+
+Usage
+-----
+
+This extension provides
---
+++ tests/TestCase.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace yii2tech\tests\unit\staticdb;
+
+use yii\helpers\ArrayHelper;
+use Yii;
+use yii\helpers\FileHelper;
+use yii2tech\tests\unit\activemail\data\Mailer;
+use yii2tech\tests\unit\activemail\data\TemplateStorage;
+use yii2tech\tests\unit\activemail\data\View;
+
+/**
+ * Base class for the test cases.
+ */
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mockApplication();
+
+        $testFilePath = $this->getTestFilePath();
+        FileHelper::createDirectory($testFilePath);
+    }
+
+    protected function tearDown()
+    {
+        $testFilePath = $this->getTestFilePath();
+        FileHelper::removeDirectory($testFilePath);
+
+        $this->destroyApplication();
+    }
+
+    /**
+     * Populates Yii::$app with a new application
+     * The application will be destroyed on tearDown() automatically.
+     * @param array $config The application configuration, if needed
+     * @param string $appClass name of the application class to create
+     */
+    protected function mockApplication($config = [], $appClass = '\yii\console\Application')
+    {
+        new $appClass(ArrayHelper::merge([
+            'id' => 'testapp',
+            'basePath' => __DIR__,
+            'vendorPath' => $this->getVendorPath(),
+            'components' => [
+                'urlManager' => [
+                    'hostInfo' => 'http://test.com',
+                    'baseUrl' => '/',
+                    'scriptUrl' => '/index.php',
+                ],
+            ],
+        ], $config));
+    }
+
+    /**
+     * @return string vendor path
+     */
+    protected function getVendorPath()
+    {
+        return dirname(__DIR__) . '/vendor';
+    }
+
+    /**
+     * Destroys application in Yii::$app by setting it to null.
+     */
+    protected function destroyApplication()
+    {
+        Yii::$app = null;
+    }
+
+    /**
+     * Returns the test file path.
+     * @return string file path.
+     */
+    protected function getTestFilePath()
+    {
+        return Yii::getAlias('@yii2tech/tests/unit/staticdb/runtime') . DIRECTORY_SEPARATOR . getmypid();
+    }
+}
