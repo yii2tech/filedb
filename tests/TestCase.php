9edf6e5a9e9f2fde813fdf251e3d8705e402ae0b
Klimov Paul <klimov.paul@gmail.com>
2017-07-03 16:41:27 +0300

    compatibility with PHPUnit < 6.x

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,7 +12,7 @@ use yii2tech\tests\unit\activemail\data\View;
 /**
  * Base class for the test cases.
  */
-class TestCase extends \PHPUnit_Framework_TestCase
+class TestCase extends \PHPUnit\Framework\TestCase
 {
     protected function setUp()
     {
